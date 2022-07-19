e661489084e2b517d8c4214744a9f150630d8d57
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-07-19 13:38:30 -0700

    fix linter erros in main.js file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-const menu_bar = document.querySelector(".toolbar");
+const menuBar= document.querySelector('.toolbar');
+const menuItems = document.querySelector('.desktop-navbar-links');
 
-const menu = document.querySelector("desktop-navbar");
-
-const menu_items = document.querySelector(".desktop-navbar-links");
-
-menu_bar.addEventListener("click", () => {
-    console.log("Menu Bar Clicked !!!");
-    menu_bar.classList.toggle("active");
-    menu_items.classList.toggle("active");
-    document.documentElement.classList.toggle('scroll-disable');
+menu_bar.addEventListener('click', () => {
+  menuBar.classList.toggle('active');
+  menuItems.classList.toggle('active');
+  document.documentElement.classList.toggle('scroll-disable');
 });
 
 // Close menu on menu-item click
-document.querySelectorAll('.nav-links').forEach(link => link.addEventListener('click', () => {
-    menu_bar.classList.remove('active');
-    menu_items.classList.remove('active');
-}))
+document.querySelectorAll('.nav-links').forEach((link) => link.addEventListener('click', () => {
+  menuBar.classList.remove('active');
+  menuItems.classList.remove('active');
+}));

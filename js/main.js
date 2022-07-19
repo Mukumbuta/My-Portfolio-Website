08eeec594c8747c794a150536b52e8d3d19f4a86
skyv26 <dev.aakashv@gmail.com>
2022-07-19 15:26:30 +0530

    update the style and javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const menu_bar = document.querySelector(".toolbar");
+
 const menu = document.querySelector("desktop-navbar");
+
 const menu_items = document.querySelector(".desktop-navbar-links");
 const actual_menu_items = document.querySelector(".actual-links");
 
 
 menu_bar.addEventListener("click", () => {
+    console.log("Menu Bar Clicked !!!");
     menu_bar.classList.toggle("active");
     menu_items.classList.toggle("active");
 });

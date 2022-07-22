7d86e6c9b0a7506b53b4c09efde64c6a55e7147d
John Kibirige <kibirigejohn005@gmail.com>
2022-07-22 09:10:09 +0300

    Use an object to store project section data and render the entire section

--- js/main.js
+++ js/main.js
@@ -14,203 +14,201 @@ document.querySelectorAll('.nav-links').forEach((link) => link.addEventListener(
   document.documentElement.classList.toggle('scroll-disable');
 }));
 
-// Modal PopUp
-const openModalBtn = document.querySelectprAll('[data-modal-target]');
-const closeModalBtn = document.querySelectorAll('[data-close-btn]');
-const overlay = document.getElementById('modal-overlay');
+// ------------------Rendering the Project section from javascript to html ----------------------/
 
-// Open Modal
-openModalBtn.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modaltarget);
-    showModal(modal);
-  });
-})
+// The first part of the section which is multi-post
+const multiPost = `
+  <article class="my-projects-multi-post">
+    <div class="multi-post-image-cont">
+      <img src="././images/project_img.png" alt="fitness picture" />
+    </div>
 
-function showModal(modal) {
-  modal.classList.add('active');
-  overlay.classList.add('active');
+    <div class="multi-post-txt-btn">
+      <h3>Multi-Post Stories</h3>
+      <p>
+        A daily selection of privately personalized reads; no accounts or
+        sign-ups required has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a standard dummy text.
+      </p>
+
+      <div class="skills-container">
+        <ul class="skills">
+          <li class="css">css</li>
+          <li class="html">html</li>
+          <li class="boot">Bootstrap</li>
+          <li class="ruby">Ruby</li>
+        </ul>
+      </div>
+
+      <button type="button" class="multi-post-btn">See Project</button>
+    </div>
+  </article>
+`;
+
+// storing all the project data in an array
+const projectData = [
+  {
+    oldTitle: 'Profesional Art Printing Data',
+    newTitle: '',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    technologies: {
+      html: 'html',
+      bootstrap: 'Bootstrap',
+      ruby: 'Ruby',
+    },
+    button: 'See Project',
+  },
+  {
+    oldTitle: 'Profesional Art Printing Data',
+    newTitle: 'Data Dashboard Health care',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    technologies: {
+      html: 'html',
+      bootstrap: 'Bootstrap',
+      ruby: 'Ruby',
+    },
+    button: 'See Projects',
+  },
+
+  {
+    oldTitle: 'Profesional Art Printing Data',
+    newTitle: 'Website Portfolio',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    technologies: {
+      html: 'html',
+      bootstrap: 'Bootstrap',
+      ruby: 'Ruby',
+    },
+    button: 'See Projects',
+  },
+
+  {
+    oldTitle: 'Profesional Art Printing Data',
+    newTitle: '',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    technologies: {
+      html: 'html',
+      bootstrap: 'Bootstrap',
+      ruby: 'Ruby',
+    },
+    button: 'See Projects',
+  },
+  {
+    oldTitle: 'Profesional Art Printing Data',
+    newTitle: 'Data Dashboard Healthcare',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    technologies: {
+      html: 'html',
+      bootstrap: 'Bootstrap',
+      ruby: 'Ruby',
+    },
+    button: 'See Projects',
+  },
+  {
+    oldTitle: 'Profesional Art Printing Data',
+    newTitle: 'Website Protfolio',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    technologies: {
+      html: 'html',
+      bootstrap: 'Bootstrap',
+      ruby: 'Ruby',
+    },
+    button: 'See Projects',
+  },
+];
+
+// looping through all these using a skeleton made up of mainly string literals
+const projects = `<div class="all-actual-projects">
+    ${projectData
+    .map(
+      (project, index) => `
+       <article class="my-projects-actual">
+          <div class=${index === 0 ? 'myp-one' : ''}>
+            <h2 class="${index === 3 ? 'adj-actual-h2' : 'old '}">${
+  project.oldTitle
+}</h2>
+            <h2 class="new adj-actual-h2 ${
+  index === 2 || index === 5 ? 'adj-actual-h3' : ''
 }
+              ${index === 4 ? 'adj-actual-h2-1' : ''} ${
+  index === 3 ? 'disappear' : ''
+}">${project.newTitle}
+              
+              </h2>
+            <p>${project.description}</p>
 
-// Close Modal
-closeModalBtn.forEach((button) => {
-  const modal = button.closest('.modal-container');
-  closeModal(modal);
-})
-function closeModal(modal) {
-  modal.classList.remove('active');
-  overlay.classList.remove('active');
-}
+            <ul class="skills1">
+              <li class="html1">${project.technologies.html}</li>
+              <li class="boot1">${project.technologies.bootstrap}</li>
+              <li class="ruby1">${project.technologies.ruby}</li>
+            </ul>
+          </div>
 
+          <button
+            type="button"
+            class="my-projects-actual-btn multi-post-btn ${
+  index === 0 ? 'myp-one-shown' : 'disappear'
+}"
+          >
+            ${project.button}
+          </button>
+       </article>
+      `,
+    )
+    .join('')}
+  </div>`;
 
+// render the entire html when the page has loaded
+window.addEventListener('load', () => {
+  const portfolioSection = `
+    <section id="portfolio" class="my-projects">
+      <div class="recent-cont">
+        <h1 class="my-projects-title">My Recent Works</h1>
+        <hr class="recent-break" />
+      </div>
+        ${multiPost}
+        ${projects}
+    </section>`;
 
+  const entireProjectsContainer = document.createElement('div');
+  entireProjectsContainer.classList.add('entire-projects-cont');
+  entireProjectsContainer.innerHTML = portfolioSection;
 
+  // insert entire project container in the right place inside the body
+  const { body } = document;
+  body.insertBefore(entireProjectsContainer, body.children[2]);
+});
 
+//   // Modal PopUp
+// const openModalBtn = document.querySelectprAll('[data-modal-target]');
+// const closeModalBtn = document.querySelectorAll('[data-close-btn]');
+// const overlay = document.getElementById('modal-overlay');
 
+// // Open Modal
+// openModalBtn.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     const modal = document.querySelector(button.dataset.modaltarget);
+//     showModal(modal);
+//   });
+// })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const multiPost = `
-//   <article class="my-projects-multi-post">
-//   <div class="multi-post-image-cont">
-//     <img src="./images/project_img.png" alt="fitness picture" />
-//   </div>
-
-//   <div class="multi-post-txt-btn">
-//     <h3>Multi-Post Stories</h3>
-//     <p>
-//       A daily selection of privately personalized reads; no accounts or
-//       sign-ups required has been the industry's standard dummy text ever
-//       since the 1500s, when an unknown printer took a standard dummy text.
-//     </p>
-
-//     <div class="skills-container">
-//       <ul class="skills">
-//         <li class="css">css</li>
-//         <li class="html">html</li>
-//         <li class="boot">Bootstrap</li>
-//         <li class="ruby">Ruby</li>
-//       </ul>
-//     </div>
-
-//     <button type="button" class="multi-post-btn">See Project</button>
-//   </div>
-//   </article>
-// `
-// // Creating the project's object
-// const projectData = {
-//   projectOne: {
-//     class: 'my-projects-actual no-overlay',
-//     title: 'Profesional Art Printing Data',
-//     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-//     technologies: {
-//       html: 'html',
-//       bootstrap: 'Bootstrap',
-//       ruby: 'Ruby'
-//     }
-//     button: 'See Project',
-//   },
-//   projectTwo: {
-//     oldTitle: 'Profesional Art Printing Data',
-//     newTitle: 'Data Dashboard Health care',
-//     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-//     technologies: {
-//       html: 'html',
-//       bootstrap: 'Bootstrap',
-//       ruby: 'Ruby'
-//     },
-//     button: 'See Projects'
-//   },
-
-//   projectThree: {
-//     oldTitle: 'Profesional Art Printing Data',
-//     newTitle: 'Website Portfolio',
-//     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-//     technologies: {
-//       html: 'html',
-//       bootstrap: 'Bootstrap',
-//       ruby: 'Ruby'
-//     },
-//     button: 'See Projects'
-//   },
-
-//   projectFour: {
-//     oldTitle: 'Profesional Art Printing Data',
-//     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-//     technologies: {
-//       html: 'html',
-//       bootstrap: 'Bootstrap',
-//       ruby: 'Ruby'
-//     },
-//     button: 'See Projects'
-//   },
-//   projectFive: {
-//     oldTitle: 'Profesional Art Printing Data',
-//     newTitle: 'Data Dashboard Healthcare',
-//     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-//     technologies: {
-//       html: 'html',
-//       bootstrap: 'Bootstrap',
-//       ruby: 'Ruby'
-//     },
-//     button: 'See Projects'
-//   },
-//   projectSix: {
-//     oldTitle: 'Profesional Art Printing Data',
-//     newTitle: 'Website Protfolio',
-//     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-//     technologies: {
-//       html: 'html',
-//       bootstrap: 'Bootstrap',
-//       ruby: 'Ruby'
-//     },
-//     button: 'See Projects'
-//   },
-
+// function showModal(modal) {
+//   modal.classList.add('active');
+//   overlay.classList.add('active');
 // }
 
-
-
-// const projects =
-//   `<div class="all-actual-projects">
-//     <article class="">
-//         <h2>Profesional Art Printing Data</h2>
-
-//         <p>
-//           A daily selection of privately personalized reads; no accounts or
-//           sign-ups required. has been the industry's standard.
-//         </p>
-
-//         <ul class="skills1">
-//           <li class="html1">html</li>
-//           <li class="boot1">Bootstrap</li>
-//           <li class="ruby1">Ruby</li>
-//         </ul>
-
-//       <button
-//         type="button"
-//         class="my-projects-actual-btn multi-post-btn myp-one-shown"
-//       >
-//         See Project
-//       </button>
-//     </article>
-//   </div>`
-
-
-// const portfolioSection = 
-//   `<section id="portfolio" class="my-projects">
-//   <div class="recent-cont">
-//     <h1 class="my-projects-title">My Recent Works</h1>
-
-//     <hr class="recent-break" />
-//     ${multiPost}
-//   </div>
-//   </section>`
+// // Close Modal
+// closeModalBtn.forEach((button) => {
+//   const modal = button.closest('.modal-container');
+//   closeModal(modal);
+// })
+// function closeModal(modal) {
+//   modal.classList.remove('active');
+//   overlay.classList.remove('active');
+// }

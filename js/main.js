38d5e27c7eeae98c31930c653d59fb4e469bd243
John Kibirige <kibirigejohn005@gmail.com>
2022-07-23 18:16:43 +0300

    Add popup menu

--- js/main.js
+++ js/main.js
@@ -8,17 +8,19 @@ menuBar.addEventListener('click', () => {
 });
 
 // Close menu on menu-item click
-document.querySelectorAll('.nav-links').forEach((link) => link.addEventListener('click', () => {
-  menuBar.classList.remove('active');
-  menuItems.classList.remove('active');
-  document.documentElement.classList.toggle('scroll-disable');
-}));
+document.querySelectorAll('.nav-links').forEach((link) =>
+  link.addEventListener('click', () => {
+    menuBar.classList.remove('active');
+    menuItems.classList.remove('active');
+    document.documentElement.classList.toggle('scroll-disable');
+  })
+);
 
 const ConvertStringToHTML = (str) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(str, 'text/html');
-    return doc.body;
-  };
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(str, 'text/html');
+  return doc.body;
+};
 
 // ------------------Rendering the Project section from javascript to html ----------------------/
 
@@ -46,7 +48,7 @@ const multiPost = `
         </ul>
       </div>
 
-      <button type="button" class="multi-post-btn">See Project</button>
+      <button type="button" class="multi-post-btn show-popup">See Project</button>
     </div>
   </article>
 `;
@@ -64,6 +66,8 @@ const projectData = [
       ruby: 'Ruby',
     },
     button: 'See Project',
+    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
+    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
   },
   {
     oldTitle: 'Profesional Art Printing Data',
@@ -76,6 +80,8 @@ const projectData = [
       ruby: 'Ruby',
     },
     button: 'See Projects',
+    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
+    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
   },
 
   {
@@ -89,6 +95,8 @@ const projectData = [
       ruby: 'Ruby',
     },
     button: 'See Projects',
+    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
+    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
   },
 
   {
@@ -102,6 +110,8 @@ const projectData = [
       ruby: 'Ruby',
     },
     button: 'See Projects',
+    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
+    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
   },
   {
     oldTitle: 'Profesional Art Printing Data',
@@ -114,6 +124,8 @@ const projectData = [
       ruby: 'Ruby',
     },
     button: 'See Projects',
+    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
+    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
   },
   {
     oldTitle: 'Profesional Art Printing Data',
@@ -126,25 +138,27 @@ const projectData = [
       ruby: 'Ruby',
     },
     button: 'See Projects',
+    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
+    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
   },
 ];
 
 // looping through all these using a skeleton made up of mainly string literals
 const projects = `<div class="all-actual-projects">
     ${projectData
-    .map(
-      (project, index) => `
+      .map(
+        (project, index) => `
        <article class="my-projects-actual">
           <div class=${index === 0 ? 'myp-one' : ''}>
             <h2 class="${index === 3 ? 'adj-actual-h2' : 'old '}">${
-  project.oldTitle
-}</h2>
+          project.oldTitle
+        }</h2>
             <h2 class="new adj-actual-h2 ${
-  index === 2 || index === 5 ? 'adj-actual-h3' : ''
-}
+              index === 2 || index === 5 ? 'adj-actual-h3' : ''
+            }
               ${index === 4 ? 'adj-actual-h2-1' : ''} ${
-  index === 3 ? 'disappear' : ''
-}">${project.newTitle}
+          index === 3 ? 'disappear' : ''
+        }">${project.newTitle}
               
               </h2>
             <p>${project.description}</p>
@@ -159,16 +173,73 @@ const projects = `<div class="all-actual-projects">
           <button
             type="button"
             class="my-projects-actual-btn multi-post-btn ${
-  index === 0 ? 'myp-one-shown' : 'disappear'
-}"
+              index === 0 ? 'myp-one-shown' : 'disappear'
+            } show-popup"
           >
             ${project.button}
           </button>
        </article>
-      `,
-    )
-    .join('')}
+      `
+      )
+      .join('')}
   </div>`;
+
+// Display Modal PopUp
+
+const descriptionMobile =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the inustry's standard dummy text ever since the 1500s, when an unknown printer took a gallery of the type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
+
+const descriptionDesktop =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.";
+
+// template for the popup object
+function generatePopup() {
+  const popupTemplate = `
+      <div id="modal" class="modal-container">
+      <h1 class="show-on-mobile">Multi Post Stories</h1>
+      <h1 class="show-on-desktop">Keeping track of hundreds  of components website</h1>
+      <img class="cross-btn" src="images/modal-images/cross-icon.svg" alt="" />
+
+      <img
+        src="./images/modal-images/popup-tags.svg"
+        alt=""
+        class="modal-popup-tag"
+      />
+
+      <div class="modal-project-container">
+        <img
+          src="images/modal-images/Snapshoot Portfolio.svg"
+          alt="Image of my first project"
+        />
+        <div class="modal-description">
+          <p class='mobile-description show-on-mobile'>
+            ${descriptionMobile}
+          </p>
+          <p class='desktop-description show-on-desktop'>
+            ${descriptionDesktop}
+          </p>
+          <div class="modal-buttons">
+            <a href="https://mukumbuta.github.io/My-Portfolio-Website/"
+              ><img
+                class="redirect-icon"
+                src="./images/modal-images/popup-seelive.svg"
+                alt=""
+            /></a>
+            <a
+              href="https://github.com/Mukumbuta/My-Portfolio-Website/tree/Modal-PopUp"
+              ><img
+                class="redirect-icon"
+                src="./images/modal-images/popup-seesource.svg"
+                alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+`;
+
+  return popupTemplate;
+}
 
 // render the entire html when the page has loaded
 window.addEventListener('load', () => {
@@ -177,91 +248,38 @@ window.addEventListener('load', () => {
   portfolioSection.appendChild(...ConvertStringToHTML(multiPost).children);
   portfolioSection.appendChild(...ConvertStringToHTML(projects).children);
   const entireProjectsContainer = document.createElement('div');
-//   entireProjectsContainer.classList.add('entire-projects-cont');
-//   entireProjectsContainer.innerHTML = portfolioSection;
 
   // insert entire project container in the right place inside the body
   const { body } = document;
   body.insertBefore(entireProjectsContainer, body.children[2]);
+
+  // popup showsup when the use clicks the show-popup buttons in multipost and the project sections
+  // we now need to add a click listener to all the buttons inside the button section
+  const showPopupButtons = document.querySelectorAll('.show-popup');
+  // loop through the buttons and add a click listener on each
+  showPopupButtons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      // toggle active class on  the overlay
+      const overlay = document.querySelector('.modal-overlay');
+      overlay.classList.toggle('active');
+      // we want to load our popup when this happens
+      const displayPopup = document.createElement('div');
+      if (index === 0) {
+        displayPopup.classList.add('display-pop-1');
+      } else {
+        displayPopup.classList.add('display-pop-2');
+      }
+      displayPopup.innerHTML = generatePopup();
+
+      // append the popup to a specific project button
+      btn.parentElement.appendChild(displayPopup);
+      btn.parentElement.style.position = 'relative';
+
+      // add an event listener to the rendered close icon
+      document.querySelector('.cross-btn').addEventListener('click', () => {
+        btn.parentElement.removeChild(displayPopup);
+        overlay.classList.toggle('active');
+      });
+    });
+  });
 });
-
-// Display Modal PopUp
-// Modal data
-const modalData = [
-  {
-    name: 'Multi-Post Stories',
-    technologies: ['css', 'html', 'Booststrap', 'Ruby'],
-    image: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    technologies: ['html', 'Booststrap', 'Ruby'],
-    image: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-  {
-    name: 'Data Dashboard Healthcare',
-    technologies: ['html', 'Booststrap', 'Ruby'],
-    image: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads;no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-  {
-    name: 'Website Protfolio',
-    technologies: ['html', 'Booststrap', 'Ruby'],
-    image: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads;no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    technologies: ['html', 'Booststrap', 'Ruby'],
-    image: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads;no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-  {
-    name: 'Data Dashboard Healthcare',
-    technologies: ['html', 'Booststrap', 'Ruby'],
-    image: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads;no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-  {
-    name: 'Website Protfolio',
-    technologies: ['html', 'Booststrap', 'Ruby'],
-    img: {
-      imageSrc: 'images/project_img.png',
-    },
-    description: 'A daily selection of privately personalized reads;no accounts or sign-ups required. has been the industrys standard',
-    Link: 'https://Mukumbuta.github.io/My-Porfolio-Website/',
-    projectRepo: 'https://github.com/Mukumbuta/My-Porfolio-Website',
-  },
-];
-
-
-const body = document.querySelector('body');
-const modalContainer = document.createElement('div');
-const secondModalContainer = document.createElement('div');
-
